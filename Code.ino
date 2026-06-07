0547981086bf9ab28f3d0d80d07432e5b9e5ebdf
archads-art <archa.sunilm@gmail.com>
2026-06-07 23:16:22 +0530

    Add files via upload

---
+++ Code.ino
@@ -0,0 +1,27 @@
+
+const int motionpin=A0;
+const int ledpin=13;
+const int buzzpin=12; // ledpin,motionpin and buzpin are not changed
+throughout the process
+int motionsensvalue=0;
+
+void setup() {
+Serial.begin(9600);
+pinMode(ledpin, OUTPUT);
+pinMode(motionpin,INPUT);
+pinMode(buzzpin,OUTPUT);
+
+}
+
+void loop() {
+  motionsensvalue=analogRead(motionpin); // reads analog data from
+motion sensor
+if (motionsensvalue>=200){
+digitalWrite(ledpin,HIGH);
+tone(buzzpin,500); //turns on led and buzzer
+}
+else {
+digitalWrite(ledpin,LOW); //turns led off led and buzzer
+noTone(buzzpin);
+}
+}
